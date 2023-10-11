a8aa9a6ee71ef21004065acf3a0a9cad322f28b1
Michael Slowik <slowik.michael@gmail.com>
2023-10-11 11:03:11 -0500

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# Laravel Sortable
+
+A simple trait to make your Laravel Eloquent models sortable with ease. 
+
+## Installation
+
+Using Composer:
+
+```bash
+composer require firevel/sortable
+```
+
+## Setup
+
+1. Import the `Sortable` trait in your Eloquent model.
+
+2. Add a protected `$sortable` array property to your model. This array should list the fields you want to allow for sorting.
+
+## Example:
+
+```php
+use Firevel\Sortable;
+
+class User extends Model {
+    use Sortable;
+
+    protected $sortable = ['id', 'name', 'email'];
+}
+```
+
+## Usage
+
+You can now easily sort your models using the `sort()` query scope.
+
+### Ascending Order:
+To sort by `name` in ascending order:
+
+```php
+User::sort(['name'])->get();
+```
+
+### Descending Order:
+To sort by `id` in descending order:
+
+```php
+User::sort(['-id'])->get();
+```
+
+The `-` sign before the field name indicates descending order.
---
+++ src/Sortable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Firevel\Sortable;
+
+use Illuminate\Support\Str;
+
+/**
+ * Trait Sortable
+ *
+ * @package Firevel\Sortable
+ */
+trait Sortable
+{
+    /**
+     * Scope a query to sort results.
+     *
+     * @param Builder $query
+     * @param array $sortingAttributes
+     * @return Builder
+     */
+    public function scopeSort($query, array $sortingAttributes)
+    {
+        $sortable = $this->sortable;
+        foreach ($sortable as $key) {
+            $sortable[] = '-' . $key; 
+        }
+        $sortingAttributes = array_intersect($sortable, $sortingAttributes);
+
+        if (empty($sortingAttributes)) {
+            return $query;
+        }
+
+        foreach ($sortingAttributes as $attribute) {
+            $sortingDirection = strpos($attribute, '-') === 0 ? 'desc' : 'asc';
+
+            $query->orderBy(Str::slug($attribute, '_'), $sortingDirection);
+        }
+
+        return $query;
+    }
+}
