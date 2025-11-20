65a15b3db1b3cb863845bde3cc5b70f57bebc05c
Michael Slowik <slowik.michael@gmail.com>
2025-11-20 10:58:55 -0500

    defaultSort handling

--- src/Sortable.php
+++ src/Sortable.php
@@ -26,9 +26,14 @@ trait Sortable
 
         $sortable = $this->sortable;
         foreach ($sortable as $key) {
-            $sortable[] = '-' . $key; 
+            $sortable[] = '-' . $key;
         }
         $sortingAttributes = array_intersect($sortable, $sortingAttributes);
+
+        // Apply default sorting if no valid sorting attributes provided
+        if (empty($sortingAttributes) && !empty($this->defaultSort)) {
+            $sortingAttributes = array_intersect($sortable, $this->defaultSort);
+        }
 
         if (empty($sortingAttributes)) {
             return $query;
@@ -36,10 +41,28 @@ trait Sortable
 
         foreach ($sortingAttributes as $attribute) {
             $sortingDirection = strpos($attribute, '-') === 0 ? 'desc' : 'asc';
+            $columnName = ltrim($attribute, '-');
 
-            $query->orderBy(Str::slug($attribute, '_'), $sortingDirection);
+            $query->orderBy(Str::slug($columnName, '_'), $sortingDirection);
         }
 
         return $query;
+    }
+
+    /**
+     * Check if a field is sortable.
+     *
+     * @param string $field
+     * @return bool
+     */
+    public function isSortable(string $field): bool
+    {
+        if (empty($this->sortable)) {
+            return false;
+        }
+
+        $field = ltrim($field, '-');
+
+        return in_array($field, $this->sortable);
     }
 }
